17de060a40cb3ee9e7ef494d0396fcd3d688a67c
Antoine Quint <antoine.quint4@gmail.com>
2025-02-06 15:52:21 +0100

    Solved all the iterations

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,29 +1,38 @@
 import { useState } from "react";
-import StudentCard from "../components/StudentCard";
+import { Link } from "react-router-dom"; 
 
 import studentsData from "../assets/students.json";
 
 function HomePage() {
-  // eslint-disable-next-line no-unused-vars
   const [students, setStudents] = useState(studentsData);
 
   return (
     <div className="border-2 border-rose-500 m-2">
-        <h1>Home Page</h1>
-        <div className="flex justify-between items-center p-2 font-bold border-b w-full">
-          <span className="flex items-center justify-center" style={{ flexBasis: "20%" }}>Image</span>
-          <span style={{ flexBasis: "20%" }}>Name</span>
-          <span style={{ flexBasis: "20%" }}>Program</span>
-          <span style={{ flexBasis: "20%" }}>Email</span>
-          <span style={{ flexBasis: "20%" }}>Phone</span>
-        </div>
+      <h1>Home Page</h1>
+      <div className="flex justify-between items-center p-2 font-bold border-b w-full">
+        <span className="flex items-center justify-center" style={{ flexBasis: "20%" }}>Image</span>
+        <span style={{ flexBasis: "20%" }}>Name</span>
+        <span style={{ flexBasis: "20%" }}>Program</span>
+        <span style={{ flexBasis: "20%" }}>Email</span>
+        <span style={{ flexBasis: "20%" }}>Phone</span>
+      </div>
 
       {students &&
-        students.map((student) => {
-          return (
-              <StudentCard key={student._id} {...student} />
-          );
-        })}
+        students.map((student) => (
+          <div key={student._id} className="flex justify-between items-center p-2 border-b">
+            <div style={{ flexBasis: "20%" }}>
+              <img src={student.image || "/path/to/placeholder.png"} alt={student.name} className="w-16 h-16 object-cover rounded-full" />
+            </div>
+            <div style={{ flexBasis: "20%" }}>
+              <Link to={`/students/${student._id}`} className="text-blue-500 hover:text-blue-700">
+                {student.firstName} {student.lastName}
+              </Link>
+            </div>
+            <div style={{ flexBasis: "20%" }}>{student.program}</div>
+            <div style={{ flexBasis: "20%" }}>{student.email}</div>
+            <div style={{ flexBasis: "20%" }}>{student.phone}</div>
+          </div>
+        ))}
     </div>
   );
 }
